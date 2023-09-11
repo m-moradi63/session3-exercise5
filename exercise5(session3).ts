4e69b73a94c8c4be251550e0d2254cb553b4b740
mostafa moradi <mostafa.moradi63.it@gmail.com>
2023-09-11 14:34:04 +0330

    exercise 5 seesion3

---
+++ exercise5(session3).ts
@@ -0,0 +1,17 @@
+const day:number = 5
+
+switch(day){
+    case 1: console.log("saturday");
+    break;
+    case 2: console.log("sunday");
+    break;
+    case 3: console.log("Monday");
+    break;
+    case 4: console.log("Tuesday");
+    break;
+    case 5: console.log("Wednesday");
+    break;
+    case 6: console.log("Tuseday");
+    break;
+    case 7: console.log("Friday");
+}
